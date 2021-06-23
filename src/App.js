353ffc52c6aa2addb6d5ef4d1e1430753911269c
Sharjith Naramparambath <sharjith@gmail.com>
2021-06-23 15:03:20 +0530

    Added ‘Delete' button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 import Header from './Header'
+import "./Header.css"
+import "./App.css"
 
 class App extends Component {
   render() {
     return (
-      <div>        
+      <div>
         <Header />
-        <button>Add</button>
-        <div>
-          <span>Name</span><br />
+        <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+            <span className="grid-item action-btn-container">
+              <button className="custom-btn delete-btn">Delete</button>
+            </span>
+          </div>
         </div>
       </div>
     );
